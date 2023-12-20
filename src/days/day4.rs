e4abca00bfa2e369e92ccebb8744070f70706879
adrienpelfresne <adrien.pelfresne@edu.esiee.fr>
2023-12-20 11:50:57 +0100

    day 4 cleanup and test

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,40 +1,36 @@
+use std::cmp::min;
+
 use crate::helper_lib::answer::Answer;
 use crate::helper_lib::solution::Solution;
 
 pub struct Day4;
 
-impl Solution for Day4 {
-    fn part_a(&self, lines: &[String]) -> Answer {
-        let mut sum = 0;
-        for line in lines {
-            let line = line.split_once(":").unwrap().1.trim();
-            let (winning, pick) = line.split_once("|").unwrap();
-            let winning = convert_to_nums(winning);
-            let pick = convert_to_nums(pick);
-            sum += 2_i32.pow((pick.iter().filter(|num| winning.contains(num)).count() - 1) as u32);
-        }
-        sum.into()
-    }
+struct Parsed {
+    cards: Vec<Card>,
+}
 
-    fn part_b(&self, lines: &[String]) -> Answer {
-        // we start with one copy of each of the cards,
-        // cards are 0 indexed : 1st card : copies[0], ...
-        let mut copies: Vec<usize> = vec![1; lines.len()];
-        for (i, &ref line) in lines.iter().enumerate() {
-            for _ in 0..copies[i] {
-                let line = line.split_once(":").unwrap().1.trim();
-                let (winning, pick) = line.split_once("|").unwrap();
-                let winning = convert_to_nums(winning);
-                let pick = convert_to_nums(pick);
-                let winning_numbers = pick.iter().filter(|num| winning.contains(num)).count();
-                for k in i..(i + winning_numbers) {
-                    copies[k + 1] += 1;
-                }
-            }
-        }
-        let sum = copies.iter().sum::<usize>();
-        sum.into()
+struct Card {
+    winning: Vec<usize>,
+    picks: Vec<usize>,
+}
+
+fn parse(input: &[String]) -> Parsed {
+    let mut cards = vec![];
+    for line in input {
+        let line = line.split_once(":").unwrap().1.trim();
+        let (winning, pick) = line.split_once("|").unwrap();
+        let winning = convert_to_nums(winning);
+        let picks = convert_to_nums(pick);
+        cards.push(Card { winning, picks });
     }
+    Parsed { cards }
+}
+
+fn count_winning(card: &Card) -> usize {
+    card.picks
+        .iter()
+        .filter(|num| card.winning.contains(num))
+        .count()
 }
 
 fn convert_to_nums(str_set: &str) -> Vec<usize> {
@@ -43,4 +39,63 @@ fn convert_to_nums(str_set: &str) -> Vec<usize> {
         nums.push(token.parse::<usize>().unwrap())
     }
     nums
+}
+
+impl Solution for Day4 {
+    fn part_a(&self, input: &[String]) -> Answer {
+        let parsed = parse(input);
+        parsed
+            .cards
+            .iter()
+            .map(|card| {
+                let count = count_winning(&card);
+                if count == 0 {
+                    return 0;
+                }
+                2_i32.pow(count as u32 - 1) as u32
+            })
+            .sum::<u32>()
+            .into()
+    }
+
+    fn part_b(&self, input: &[String]) -> Answer {
+        // we start with one copy of each of the cards,
+        // cards are 0 indexed : 1st card : copies[0], ...
+        let parsed = parse(input);
+        let mut copies: Vec<usize> = vec![1; parsed.cards.len()];
+        parsed.cards.iter().enumerate().for_each(|(i, card)| {
+            let winning_count = count_winning(&card);
+            for _ in 0..copies[i] {
+                copies
+                    .iter_mut()
+                    .skip(i + 1)
+                    .take(winning_count)
+                    .for_each(|copy| *copy += 1);
+            }
+        });
+        copies.iter().sum::<usize>().into()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::helper_lib::{self, answer::Answer, input, solution::Solution};
+
+    use super::Day4;
+
+    #[test]
+    pub fn test_a() {
+        let input =
+            input::read_file(&format!("{}day_4_test.txt", helper_lib::FILES_PREFIX)).unwrap();
+        let answer = Day4.part_a(&input);
+        assert_eq!(<i32 as Into<Answer>>::into(13i32), answer);
+    }
+
+    #[test]
+    pub fn test_b() {
+        let input =
+            input::read_file(&format!("{}day_4_test.txt", helper_lib::FILES_PREFIX)).unwrap();
+        let answer = Day4.part_b(&input);
+        assert_eq!(<i32 as Into<Answer>>::into(30i32), answer);
+    }
 }
