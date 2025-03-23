8f368950ab10aa2b830520d44faf18ac702bb63a
dirdr <adrien.pelfresne@gmail.com>
2025-03-23 11:26:10 +0100

    solution(2024): day 17 part b solution + cleanup

--- aoc_2024/src/day17.rs
+++ aoc_2024/src/day17.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use aoc_lib::{answer::Answer, solution::Solution};
 use itertools::Itertools;
 
@@ -5,12 +7,61 @@ pub struct Day17;
 
 impl Solution for Day17 {
     fn part_a(&self, input: &[String]) -> Answer {
-        Computer::from_input(input).run_program().into()
+        let (computer, mut registers) = parse_input(input);
+        let out = computer.run_program(&mut registers);
+
+        out.iter()
+            .map(|token| token.to_string())
+            .collect::<Vec<String>>()
+            .join(",")
+            .into()
     }
 
     fn part_b(&self, input: &[String]) -> Answer {
-        Computer::from_input(input).part_b().into()
+        let (computer, _) = parse_input(input);
+        let program_len = computer.raw_program.len();
+        let mut to_visit = VecDeque::from([(program_len, 0)]);
+
+        while let Some((pos, a)) = to_visit.pop_front() {
+            for i in 0..8 {
+                let n_a = a * 8 + i;
+                let out = computer.run_program(&mut [n_a, 0, 0]);
+                if out == computer.raw_program[pos - 1..] {
+                    to_visit.push_back((pos - 1, n_a));
+                    if out.len() == program_len {
+                        return n_a.into();
+                    }
+                }
+            }
+        }
+        Answer::Unimplemented
     }
+}
+
+fn parse_input(input: &[String]) -> (Computer, [u64; 3]) {
+    let mut instructions = vec![];
+    let (regs, program) = input.split(|l| l.is_empty()).collect_tuple().unwrap();
+    let reg_a = regs[0][12..].parse::<u64>().unwrap();
+    let reg_b = regs[1][12..].parse::<u64>().unwrap();
+    let reg_c = regs[2][12..].parse::<u64>().unwrap();
+
+    let (_, program) = program[0].split_once(' ').unwrap();
+
+    let raw_program = program
+        .split(',')
+        .map(|e| e.parse::<u8>().unwrap())
+        .collect::<Vec<_>>();
+
+    for token in raw_program.chunks(2) {
+        instructions.push(Instruction::from_consecutive_tokens(token[0], token[1]))
+    }
+    (
+        Computer {
+            instructions,
+            raw_program,
+        },
+        [reg_a, reg_b, reg_c],
+    )
 }
 
 enum Instruction {
@@ -25,60 +76,29 @@ enum Instruction {
 }
 
 struct Computer {
-    registers: [u32; 3],
     instructions: Vec<Instruction>,
+    raw_program: Vec<u8>,
 }
 
 impl Computer {
-    fn from_input(input: &[String]) -> Self {
-        let mut instructions = vec![];
-        let (regs, program) = input.split(|l| l.is_empty()).collect_tuple().unwrap();
-        let reg_a = regs[0][12..].parse::<u32>().unwrap();
-        let reg_b = regs[1][12..].parse::<u32>().unwrap();
-        let reg_c = regs[2][12..].parse::<u32>().unwrap();
-
-        let (_, program) = program[0].split_once(' ').unwrap();
-
-        for token in program
-            .split(',')
-            .map(|e| e.parse::<u8>().unwrap())
-            .collect::<Vec<_>>()
-            .chunks(2)
-        {
-            instructions.push(Instruction::from_consecutive_tokens(token[0], token[1]))
-        }
-        Self {
-            registers: [reg_a, reg_b, reg_c],
-            instructions,
-        }
-    }
-
-    fn run_program(mut self) -> String {
+    fn run_program(&self, registers: &mut [u64; 3]) -> Vec<u8> {
         let mut out = vec![];
         let mut instruction_pointer = 0;
 
         while instruction_pointer < self.instructions.len() {
             let instruction = &self.instructions[instruction_pointer];
             if let Instruction::Jnz(_) = instruction {
-                if let Some(new_pointer) = instruction.run_side_effect(&self.registers) {
+                if let Some(new_pointer) = instruction.run_side_effect(registers) {
                     instruction_pointer = new_pointer;
                 } else {
                     instruction_pointer += 1;
                 }
             } else {
-                instruction.run(&mut self.registers, &mut out);
+                instruction.run(registers, &mut out);
                 instruction_pointer += 1;
             }
         }
-
-        out.iter()
-            .map(|token| token.to_string())
-            .collect::<Vec<String>>()
-            .join(",")
-    }
-
-    fn part_b(mut self) -> u64 {
-        todo!()
+        out
     }
 }
 
@@ -97,7 +117,7 @@ impl Instruction {
         }
     }
 
-    fn run(&self, registers: &mut [u32; 3], out: &mut Vec<u8>) {
+    fn run(&self, registers: &mut [u64; 3], out: &mut Vec<u8>) {
         match self {
             Instruction::Adv(o) => Self::adv(registers, *o),
             Instruction::Bxl(o) => Self::bxl(registers, *o),
@@ -110,14 +130,14 @@ impl Instruction {
         }
     }
 
-    fn run_side_effect(&self, registers: &[u32; 3]) -> Option<usize> {
+    fn run_side_effect(&self, registers: &[u64; 3]) -> Option<usize> {
         if let Self::Jnz(o) = self {
             return Self::jnz(registers, *o);
         }
         unreachable!()
     }
 
-    fn adv(registers: &mut [u32; 3], operand: u8) {
+    fn adv(registers: &mut [u64; 3], operand: u8) {
         let num = registers[0];
         let denominator = match operand {
             n @ 0..=3 => 1 << n,
@@ -133,13 +153,13 @@ impl Instruction {
         registers[0] = result;
     }
 
-    fn bxl(registers: &mut [u32; 3], operand: u8) {
-        registers[1] ^= operand as u32;
+    fn bxl(registers: &mut [u64; 3], operand: u8) {
+        registers[1] ^= operand as u64;
     }
 
-    fn bst(registers: &mut [u32; 3], operand: u8) {
+    fn bst(registers: &mut [u64; 3], operand: u8) {
         registers[1] = match operand {
-            n @ 0..=3 => n as u32,
+            n @ 0..=3 => n as u64,
             4 => registers[0],
             5 => registers[1],
             6 => registers[2],
@@ -150,20 +170,20 @@ impl Instruction {
         } % 8;
     }
 
-    fn jnz(registers: &[u32; 3], operand: u8) -> Option<usize> {
+    fn jnz(registers: &[u64; 3], operand: u8) -> Option<usize> {
         if registers[0] != 0 {
             return Some(operand as usize);
         }
         None
     }
 
-    fn bxc(registers: &mut [u32; 3]) {
+    fn bxc(registers: &mut [u64; 3]) {
         registers[1] ^= registers[2];
     }
 
-    fn out(output: &mut Vec<u8>, registers: &[u32; 3], operand: u8) {
-        let token: u32 = match operand {
-            n @ 0..=3 => n as u32,
+    fn out(output: &mut Vec<u8>, registers: &[u64; 3], operand: u8) {
+        let token: u64 = match operand {
+            n @ 0..=3 => n as u64,
             4 => registers[0],
             5 => registers[1],
             6 => registers[2],
@@ -175,7 +195,7 @@ impl Instruction {
         output.push((token % 8) as u8);
     }
 
-    fn bdv(registers: &mut [u32; 3], operand: u8) {
+    fn bdv(registers: &mut [u64; 3], operand: u8) {
         let denominator = match operand {
             n @ 0..=3 => 1 << n,
             4 => 1 << registers[0],
@@ -190,7 +210,7 @@ impl Instruction {
         registers[1] = result;
     }
 
-    fn cdv(registers: &mut [u32; 3], operand: u8) {
+    fn cdv(registers: &mut [u64; 3], operand: u8) {
         let denominator = match operand {
             n @ 0..=3 => 1 << n,
             4 => 1 << registers[0],
@@ -215,15 +235,18 @@ mod test {
     #[test]
     fn test_a() {
         let input =
-            input::read_file(&format!("{}day_17_test.txt", crate::FILES_PREFIX_TEST)).unwrap();
+            input::read_file(&format!("{}day_17_a_test.txt", crate::FILES_PREFIX_TEST)).unwrap();
         let answer = Day17.part_a(&input);
-        assert_eq!(<i32 as Into<Answer>>::into(11), answer);
+        assert_eq!(
+            <String as Into<Answer>>::into("4,6,3,5,6,3,5,2,1,0".to_string()),
+            answer
+        );
     }
 
     #[test]
     fn test_b() {
         let input =
-            input::read_file(&format!("{}day_17_test.txt", crate::FILES_PREFIX_TEST)).unwrap();
+            input::read_file(&format!("{}day_17_b_test.txt", crate::FILES_PREFIX_TEST)).unwrap();
         let answer = Day17.part_b(&input);
         assert_eq!(<i32 as Into<Answer>>::into(117440), answer);
     }
